dfdc3846c260dcc19c33f8d313b554b18c52b4e6
Aidan Williamson <aiwilliamson@microsoft.com>
2023-01-20 16:54:47 +0000

    initial creation w/ poetry

---
+++ azure_data-scientist/playaround_visualise_data.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Load data from a text file
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/grades.csv
+df_students = pd.read_csv('grades.csv',delimiter=',',header='infer')
